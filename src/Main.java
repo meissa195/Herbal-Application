87d123ccd10ad64dcf128e80b364c1a79c80ec4c
Mohamed <mgeissa13@gmail.com>
2021-11-29 15:27:39 -0800

    Further revisions to the structure of the application; next step separate into classes

--- src/Main.java
+++ src/Main.java
@@ -9,49 +9,58 @@ public class Main {
 
     public static void main(String[] args) {
         UserInterface input = new UserInterface();
-        input.setHerbs();
         input.askWhichHerb();
-
     }
 
+    //why do I have to make this static?
     static class UserInterface {
-        private ArrayList<Object> herbs;
-        private HashMap<String, Object> herbDatabase;
+        private ArrayList<Object> herbQuality;
+        private ArrayList<Object> herbAction;
+        private HashMap<String, Object> herbQualities;
+        private HashMap<String, Object> herbActions;
 
 
         public UserInterface() {
-            this.herbs = new ArrayList<>();
-            this.herbDatabase = new HashMap<>();
-
+            this.herbQuality = new ArrayList<>();
+            this.herbAction = new ArrayList<>();
+            this.herbQualities = new HashMap<>();
+            this.herbActions = new HashMap<>();
         }
 
         //add herbs to database
         public void setHerbs() {
-            //first add herbs to a list
-            herbs.add(new Garlic().qualities());
-            herbs.add("Anise");
-            herbs.add("Chamomile");
-            herbs.add("Ginger");
-            herbs.add("Fenugreek");
+            //quality list for herbs
+            herbQuality.add(new Garlic().qualities());
+
+            //actions list for herbs
+            herbAction.add(new Garlic().actions());
 
             //use hashmap to reference list
-            herbDatabase.put("garlic", herbs.get(0)); // should return Garlic, where it will contain actions and qualities
+            herbQualities.put("qualities", herbQuality.get(0)); // should return Garlic, where it will contain actions and qualities
+            herbActions.put("actions", herbAction.get(0)); // should return Garlic, where it will contain actions and qualities
         }
 
         //user interface that asks for herb of interest?
         public void askWhichHerb() {
+            setHerbs();
             System.out.println("What herb would you like to learn about?");
             Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine().toLowerCase();
-            if (!input.isEmpty()) { //how do I set it up where if they input anything, it returns what they are looking for?
-                System.out.println("What would you like to learn about the herb?");
-                String response = scanner.nextLine();
-                if (response.equalsIgnoreCase("qualities")) {
-                    System.out.println(herbDatabase.get(input));
+            String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("garlic")) {
+                    //how do I set it up where if they input anything, it returns what they are looking for?
+                    System.out.println("What would you like to learn about?\n>Actions\n>Qualities");
+                    System.out.println();
+                    String response = scanner.nextLine().toLowerCase();
+                    if (herbQualities.containsKey(response)) {
+                        System.out.println(herbQualities.get(response));
+                    } else if (herbActions.containsKey(response)) {
+                        System.out.println(herbActions.get(response));
+                    } else
+                        System.out.println("No qualities or actions found.");
+
                 } else
-                    System.out.println("No qualities found.");
-            } else
-                System.out.println("Herb not found.");
+                        System.out.println("Herb not found.");
+
 
 //                //then asks what would you like to know the herb?
 //                ArrayList<String> qualities = new ArrayList<>();
@@ -60,33 +69,52 @@ public class Main {
 //                qualities.add("Dry");
 //                qualities.add("Moist");
 
-        }
-    }
+            }
+
+        static class Garlic {
+            private String garlic;
+            private ArrayList<String> qualities; //every herb has qualities
+            private ArrayList<String> actions; //every herb has actions
 
 
-    static class Garlic {
-        private String garlic;
-        private ArrayList<String> qualities; //every herb has qualities
-        private ArrayList<String> actions; //every herb has actions
+            //should I make the actions and qualities interfaces?
+            public Garlic() {
+                this.garlic = "Garlic";
 
-        public Garlic() {
-            this.garlic = "Garlic";
+                //list of qualities
+                this.qualities = new ArrayList<>();
+                qualities.add("Hot");
+                qualities.add("Cold");
+                qualities.add("Dry");
+                qualities.add("Moist");
 
-            //list of qualities
-            this.qualities = new ArrayList<>();
-            qualities.add("Hot");
-            qualities.add("Cold");
-            qualities.add("Dry");
-            qualities.add("Moist");
+                //list of actions
+                this.actions = new ArrayList<>();
+                actions.add("Adaptogen");
+                actions.add("Astringent");
+                actions.add("Bitter");
+                actions.add("Cardiac Remedy");
+                actions.add("Cholagogue");
+                actions.add("Demulcent");
+                actions.add("Diuretic");
+                actions.add("Emmenagogue");
+                actions.add("Expectorant");
+                actions.add("Hepatic");
+                actions.add("Nervine");
+                actions.add("Sedative");
+            }
 
-            //list of actions
-            this.actions = new ArrayList<>();
-        }
+            public String qualities() {
+                return "The herb qualities are:\n "
+                        + qualities.get(0) + " and "
+                        + qualities.get(2) + ".";
+            }
 
-        public String qualities() {
-            return "The herbs qualities are:\n "
-                    + qualities.get(0) + " and "
-                    + qualities.get(2) + ".";
+            public String actions() {
+                return "The herb actions are:\n "
+                        + actions.get(0) + " and "
+                        + actions.get(2) + ".";
+            }
         }
     }
 }
